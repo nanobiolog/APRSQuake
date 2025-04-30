ac5ac73b0ce15191f3a6e39c573619c04fce1cda
ATIL YALI <atilihsan38@gmail.com>
2025-04-30 19:47:15 +0300

    first commit

---
+++ app/src/main/java/com/ta1csf/aprsquake/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.ta1csf.aprsquake
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.ta1csf.aprsquake.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Example of a call to a native method
+        binding.sampleText.text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'aprsquake' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+        // Used to load the 'aprsquake' library on application startup.
+        init {
+            System.loadLibrary("aprsquake")
+        }
+    }
+}
